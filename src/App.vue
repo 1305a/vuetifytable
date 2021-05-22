7201bc25394cc9beb7ded6d2180033ae011a1a58
1305a <1305andrew2@gmail.com>
2021-05-22 14:20:45 +0300

    use vuetify table

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
   <v-app>
-   <v-btn
-   @click ="tableFunctionMethods"
-   >
-   Кнопка
-   </v-btn>
-   <div
-    v-for="item in tableFunction"
-    :key="item.id"
-   >
-   {{item.name}}
-   </div>
+    <v-data-table
+      :headers="headers"
+      :items="tableFunction"
+      :items-per-page="10"
+      class="elevation-1"
+    >
+    </v-data-table>
   </v-app>
 </template>
 
@@ -23,10 +19,18 @@ export default {
 
   components: {
   },
+  data() {
+    return {
+      headers: [{text:'NAME',value: 'name', width: '17rem'},
+      {text:'DESCRIPTION',value: 'description', width: '45%'},
+      {text:'MIME TYPE',value: 'mime_type', width: '27%'},
+      {text:'OPEN MODE',value: 'open_mode', width: '10.3rem'}
+      ]
+    }
+  },
   computed: {
     tableFunction() {
       return tableJson
-      //tableData = tableJson
     }
     
   },
